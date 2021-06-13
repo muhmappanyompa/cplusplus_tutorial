95fe1f443e0deb7a63333ec0bfef58637c5ebbfd
Muhammad Mappanyompa <defcoolz@gmail.com>
2021-06-13 23:45:07 +0900

    Add files via upload

---
+++ python/VS Code Python/DataCamp/print.py
@@ -0,0 +1,16 @@
+# Example, do not modify!
+print(5 / 8)
+
+# Put code below here
+print(7 +10)
+
+#Addition, substraction
+print (5+5)
+print (6-9)
+
+#Multiplication, division, modulo, exponentiation
+print(6*5)
+print(8/4)
+print(20%3)
+print(4**2)
+
